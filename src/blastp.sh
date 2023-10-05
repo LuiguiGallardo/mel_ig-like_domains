49202071d79b0ee2b23dd8877abab55913f81fc8
LuiguiGallardo <luiguimichelgallardo@gmail.com>
2023-10-05 04:55:48 -0700

    Contigs tables created

--- src/blastp.sh
+++ src/blastp.sh
@@ -25,7 +25,7 @@ for ((perc=50; perc <= 100; perc +=5))
     echo "cat ../03_blastp/blastp_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_$perc.tsv | cut -f 1 | sort | uniq | wc -l "
 done | bash >> ../03_blastp/blastp_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_values.tsv
 
-# Create table contigs
+# Create table count contigs
 touch ../03_blastp/../03_blastp/blastp_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_contigs.tsv
 
 printf "cov-id\tcontigs\n" > ../03_blastp/blastp_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_contigs.tsv
@@ -34,3 +34,8 @@ for ((perc=50; perc <= 100; perc +=5))
     do echo "printf "cov-id_"$perc'\t'"
     echo "cat ../03_blastp/blastp_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_$perc.tsv | cut -f 1 | cut -f -2 -d _ | sort | uniq | wc -l "
 done | bash >> ../03_blastp/blastp_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_contigs.tsv
+
+# Create tables contigs
+for ((perc=50; perc <= 100; perc +=5))
+    do echo "cat ../03_blastp/blastp_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_$perc.tsv | cut -f 1 | cut -f -2 -d _ | sort | uniq > ../03_blastp/contigs_prodigal_Fagoma_Raton_5498_outfmt6_cov-id_$perc.tsv"
+done | bash
